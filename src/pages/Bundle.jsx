65b808049fbfe54e3f5d745196b7e4cafc414b74
edycoleee <edycoleee@gmail.com>
2023-12-29 06:43:31 +0700

    bundle oke

--- src/pages/Bundle.jsx
+++ src/pages/Bundle.jsx
@@ -12,7 +12,6 @@ function Bundle() {
     NoIHS: "P00590441751",
     NoRegister: "RG2023I0000175",
     TglJam: "2023-12-27T05:57:41-07:00",
-    DiagAwal: "batuk pilek",
     DiagUtama: "K35.8",
     NamaDiagUtama: "Acute appendicitis, other and unspecified",
     Temp: 36,
@@ -30,7 +29,6 @@ function Bundle() {
     }))
   }
   const uuidEncounter = uuidv4()
-  const uuidKeluhan = uuidv4()
   const uuidCondition = uuidv4()
   const uuidObservation = uuidv4()
 
@@ -83,24 +81,7 @@ function Bundle() {
               }
             }
           ],
-          "diagnosis": [
-            {
-              "condition": {
-                "reference": "urn:uuid:" + uuidKeluhan,
-                "display": dtSend.DiagAwal
-              },
-              "use": {
-                "coding": [
-                  {
-                    "system": "http://terminology.hl7.org/CodeSystem/diagnosis-role",
-                    "code": "DD",
-                    "display": "Discharge diagnosis"
-                  }
-                ]
-              },
-              "rank": 1
-            }
-          ],
+
           "statusHistory": [
             {
               "status": "arrived",
@@ -124,18 +105,6 @@ function Bundle() {
               }
             }
           ],
-          "hospitalization": {
-            "dischargeDisposition": {
-              "coding": [
-                {
-                  "system": "http://terminology.hl7.org/CodeSystem/discharge-disposition",
-                  "code": "home",
-                  "display": "Home"
-                }
-              ],
-              "text": ""
-            }
-          },
           "serviceProvider": {
             "reference": "Organization/" + dtSend.OrganisationID
           },
@@ -190,7 +159,7 @@ function Bundle() {
           },
           "encounter": {
             "reference": "urn:uuid:" + uuidEncounter,
-            "display": "Kunjungan" + dtSend.NamaPasien + "di" + dtSend.TglJam
+            "display": "Kunjungan " + dtSend.NamaPasien + " di " + dtSend.TglJam
           }
         },
         "request": {
@@ -198,64 +167,74 @@ function Bundle() {
           "url": "Condition"
         }
       },
-      // {
-      //   "fullUrl": "urn:uuid:" + uuidObservation,
-      //   "resource": {
-      //     "resourceType": "Observation",
-      //     "status": "final",
-      //     "category": [
-      //       {
-      //         "coding": [
-      //           {
-      //             "system": "http://terminology.hl7.org/CodeSystem/observation-category",
-      //             "code": "vital-signs",
-      //             "display": "Vital Signs"
-      //           }
-      //         ]
-      //       }
-      //     ],
-      //     "code": {
-      //       "coding": [
-      //         {
-      //           "system": "http://loinc.org",
-      //           "code": "8310-5",
-      //           "display": "Body temperature"
-      //         }
-      //       ]
-      //     },
-      //     "performer": [
-      //       {
-      //         "reference": "Practitioner/" + dtSend.PractisionerIHS
-      //       }
-      //     ],
-      //     "subject": {
-      //       "reference": "Patient/" + dtSend.PatientIHS
-      //     },
-      //     "encounter": {
-      //       "reference": "urn:uuid:" + uuidEncounter,
-      //       "display": "Kunjungan" + dtSend.NamaPasien + "di" + dtSend.TglJam
-      //     },
-      //     "effectiveDateTime": dtSend.TglJam,
-      //     "issued": dtSend.TglJam,
-      //     "valueQuantity": {
-      //       "value": dtSend.Temp,
-      //       "unit": "Cel",
-      //       "system": "http://unitsofmeasure.org",
-      //       "code": "Cel"
-      //     }
-      //   },
-      //   "request": {
-      //     "method": "POST",
-      //     "url": "Observation"
-      //   }
-      // },
+      {
+        "fullUrl": "urn:uuid:" + uuidObservation,
+        "resource": {
+          "resourceType": "Observation",
+          "status": "final",
+          "category": [
+            {
+              "coding": [
+                {
+                  "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+                  "code": "vital-signs",
+                  "display": "Vital Signs"
+                }
+              ]
+            }
+          ],
+          "code": {
+            "coding": [
+              {
+                "system": "http://loinc.org",
+                "code": "8310-5",
+                "display": "Body temperature"
+              }
+            ]
+          },
+          "subject": {
+            "reference": "Patient/" + dtSend.NoIHS
+          },
+          "performer": [
+            {
+              "reference": "Practitioner/" + dtSend.PractisionerIHS,
+            }
+          ],
+          "encounter": {
+            "reference": "urn:uuid:" + uuidEncounter,
+            "display": "Kunjungan " + dtSend.NamaPasien + " di " + dtSend.TglJam
+          },
+          "effectiveDateTime": dtSend.TglJam,
+          "valueQuantity": {
+            "value": dtSend.Temp,
+            "unit": "C",
+            "system": "http://unitsofmeasure.org",
+            "code": "Cel"
+          },
+          // "interpretation": [
+          //   {
+          //     "coding": [
+          //       {
+          //         "system": "http://terminology.hl7.org/CodeSystem/v3-ObservationInterpretation",
+          //         "code": "H",
+          //         "display": "High"
+          //       }
+          //     ],
+          //     "text": "Di atas nilai referensi"
+          //   }
+          // ]
+        },
+        "request": {
+          "method": "POST",
+          "url": "Observation"
+        }
+      }
     ]
   }
 
-
   const postBundle = () => {
     setLoading(true)
-    console.log("POST Bundle");
+    console.log("POST Bundle", DATABUNDLE);
 
     axios({
       url: "/api/fhir-r4/v1",
@@ -285,6 +264,7 @@ function Bundle() {
       });
 
   }
+  //bf060ae3-1d58-4994-885a-3eba3f4ca118
 
   return (
     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 m-2">
@@ -318,7 +298,7 @@ function Bundle() {
           NoRegister :
         </label>
         <input
-          className="shadow appearance-none border rounded w-1/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="NoRegister"
           type="text"
           name="NoRegister"
@@ -329,22 +309,11 @@ function Bundle() {
           TglJam :
         </label>
         <input
-          className="shadow appearance-none border rounded w-1/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="TglJam"
           type="text"
           name="TglJam"
           value={dtSend.TglJam}
-          onChange={handleChange}
-        />
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="DiagAwal">
-          DiagAwal :
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="DiagAwal"
-          type="text"
-          name="DiagAwal"
-          value={dtSend.DiagAwal}
           onChange={handleChange}
         />
         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="DiagUtama">
@@ -388,6 +357,15 @@ function Bundle() {
         <div className="m-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
             BUNDLE RESPONSE : {resBundle === "" ? "BELUM ADA RESPONSE" : JSON.stringify(resBundle)}
+          </label>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+            RESOURCE ID ENCOUNTER : {resBundle === "" ? "BELUM ADA RESPONSE" : JSON.stringify(resBundle.entry[0]?.response.resourceID)}
+          </label>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+            RESOURCE ID CONDITION : {resBundle === "" ? "BELUM ADA RESPONSE" : JSON.stringify(resBundle.entry[1]?.response.resourceID)}
+          </label>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+            RESOURCE ID OBSERVATION : {resBundle === "" ? "BELUM ADA RESPONSE" : JSON.stringify(resBundle.entry[2]?.response.resourceID)}
           </label>
         </div>
       )}
